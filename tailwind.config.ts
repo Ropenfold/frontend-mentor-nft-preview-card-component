0333a556230373495c6a8455740b308335c81c47
Stuart Roper <stuart.roper@bravissimo.com>
2024-10-17 16:49:46 +0100

    add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'], 
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",

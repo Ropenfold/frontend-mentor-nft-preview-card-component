c7dd510d58a6053cdf17ca933225878742b9f307
Stuart Roper <stuart.roper@bravissimo.com>
2024-10-18 10:21:18 +0100

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/frontend-mentor-nft-preview-card-component', 
+  assetPrefix: '/frontend-mentor-nft-preview-card-component/', 
+  output: 'export',
+  reactStrictMode: true, 
     images: {
-        domains: ['frontend-mentor-nft-preview-card-component']
+        domains: ['frontend-mentor-nft-preview-card-component.com']
     }
 };
 

42adc0f3fab6ac266ddbb1dd2696383cb3b05507
Stuart Roper <stuart.roper@bravissimo.com>
2024-10-21 17:16:32 +0100

    update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+// next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // Set basePath and assetPrefix based on your GitHub repository name
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
     basePath: '/frontend-mentor-nft-preview-card-component', 
   assetPrefix: '/frontend-mentor-nft-preview-card-component/', 
+  assetPrefix,
+  basePath,
   output: 'export',
   reactStrictMode: true, 
     images: {
+      unoptimized: true,
         domains: ['frontend-mentor-nft-preview-card-component']
     }
 };

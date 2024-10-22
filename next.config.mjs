8e9061b98d67f5b48070f0f2c1a0bab0c8e3bab7
Stuart Roper <stuart.roper@bravissimo.com>
2024-10-22 09:22:24 +0100

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,27 @@
 /** @type {import('next').NextConfig} */
 
-// next.config.js
+// Detect if running on GitHub Actions
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 let assetPrefix = '';
 let basePath = '';
 
 if (isGithubActions) {
-  // Set basePath and assetPrefix based on your GitHub repository name
+  // Get the repository name for use as the base path and asset prefix
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
 }
 
 const nextConfig = {
-    basePath: '/frontend-mentor-nft-preview-card-component', 
-  assetPrefix: '/frontend-mentor-nft-preview-card-component/', 
-  assetPrefix,
-  basePath,
-  output: 'export',
-  reactStrictMode: true, 
-    images: {
-      unoptimized: true,
-        domains: ['frontend-mentor-nft-preview-card-component']
-    }
+  assetPrefix, // Use assetPrefix determined by GitHub environment
+  basePath,    // Use basePath determined by GitHub environment
+  output: 'export',  // Enable static export
+  reactStrictMode: true, // Optional, but recommended for debugging
+
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for static export
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
